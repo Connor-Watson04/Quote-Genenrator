531847420e7045356ca535a92bf3fcd65e0211a6
Connor Watson <connorjosephwatson@hotmail.com>
2024-05-14 17:08:34 +0100

    applying class for quotes that are too big and making authors unknown if author = null

--- Script.js
+++ Script.js
@@ -1,12 +1,33 @@
-//https://jacintodesign.github.io/quotes-api/data/quotes.json
+//
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
+// Show new quote - using API
 let apiQuotes = [];
 
-// Show new quote
 function newQuote() {
   // pick a random quote from the apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  //check IF author field is blank and replace with 'unknown'
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  //Check for quote length to determine styling
+
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -24,3 +45,23 @@ async function getQuotes() {
 
 // On Load
 getQuotes();
+
+//Show new quote - Local data
+
+//function newQuote() {
+// pick a random quote from the apiQuotes array
+//  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+// console.log(quote);
+//}
+//async function getQuotes() {
+//    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+//    try {
+//      //only sets the response when theres actual data
+//      const response = await fetch(apiUrl);
+//      apiQuotes = await response.json();
+//      newQuote();
+//    } catch (error) {
+//      // catch error here
+//    }
+//  }
+//newQuote();
